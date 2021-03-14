3cc55c20934223e6d1b7546afdaeed23fbb11b61
mnjoyanx <tigranmnjoyan@gmail.com>
2021-03-14 16:39:46 +0400

    sql requests

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,8 +9,24 @@ class HomeController extends Controller
 {
     public function index()
     {
+        // DB::insert('INSERT INTO posts (title, content, created_at, updated_at) VALUES (?, ?, ?, ?)', ['title 1', 'this is content 1', now(), now()]);
 
-        $all_users = DB::select('SELECT * from users');
-        return view('welcome', ['all_users' => $all_users]);
+        // DB::update('UPDATE posts SET  created_at = ?, updated_at = ? WHERE created_at IS NULL OR updated_at IS NULL', [now(), now()]);
+
+        // DB::delete('DELETE FROM posts WHERE id = :id', [3]);
+
+
+        DB::beginTransaction();
+
+        try {
+            DB::update('UPDATE posts SET content = ?', ['this is content four']);
+            DB::commit();
+        } catch(\Exception $e) {
+            DB::rollBack();
+            echo $e->getMessage();
+        }
+
+        $posts = DB::select("SELECT * FROM posts WHERE id < 3");
+        return $posts;
     }
 }
